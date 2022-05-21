26cebf18d16ac69e756c7a8e2ec5c5cca73e0ddf
dedovds <dedov.ds@phystech.edu>
2022-05-21 03:56:35 +0300

    added interface of vector

--- vector.h
+++ vector.h
@@ -14,5 +14,32 @@ typedef enum {
     EINDEX,
     EINVARG
 } VECTOR_ERR;
+
+Vector* init_v(VECTOR_ERR* err, int size);
+
+void remove_v(Vector* v, VECTOR_ERR* err);
+
+void extend(Vector* v, VECTOR_ERR* err);
+
+int size(Vector* v, VECTOR_ERR* err);
+
+int capacity(Vector* v, VECTOR_ERR* err);
+
+void clear(Vector* v, VECTOR_ERR* err);
+
+void push_back(Vector* v, VECTOR_ERR* err, int element);
+
+int at(Vector* v, VECTOR_ERR* err, int index);
+
+void pop_back(Vector* v, VECTOR_ERR* err);
+
+void print(Vector* v);
+
+void find(Vector* v, VECTOR_ERR* err, int elem);
+
+int empty(Vector* v, VECTOR_ERR* err);
+
+int back(Vector* v, VECTOR_ERR* err);
+
 #endif
 
