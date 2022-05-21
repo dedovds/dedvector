94c4d7ad74960d6eb8f1f63f9a0ae4c198ed5658
dedovds <dedov.ds@phystech.edu>
2022-05-21 11:47:46 +0300

    Fix makefile

--- tests.c
+++ tests.c
@@ -19,5 +19,44 @@ int main() {
   
   vec = init_v(&err, 25);
   test_error(err, ESUCCESS, 2);
+	
+  vec = NULL;
+  remove_v(vec, &err);
+  test_error(err, EINVARG, 3);
+
+  int a = size(vec, &err);
+  test_error(err, EINVARG, 4);
+
+  extend(vec, &err);
+  test_error(err, EINVARG, 5);
+
+  int b = capacity(vec, &err);
+  test_error(err, EINVARG, 6);
+
+  clear(vec, &err);
+  test_error(err, EINVARG, 7);
+
+  push_back(vec, &err, 5);
+  test_error(err, EINVARG, 8);
+
+  int c = at(vec, &err, 5);
+  test_error(err, EINVARG, 9);
+
+  pop_back(vec, &err);
+  test_error(err, EINVARG, 10);
+
+  print(vec, &err);
+  tesr_error(err, EINVARG, 11);
+
+  int d = find(vec, &err, 5);
+  test_error(err, EINVARG, 12);
+
+  int e = back(vec, &err);
+  test_error(err, EINVARG, 13);
+
+  int f = empty(vec, &err);
+  test_error(err, EINVARG, 14);
+
+
 }
 
